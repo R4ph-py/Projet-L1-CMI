dc9a39bba18eac508d6683057d1d1847d350625b
R4ph-py <52760811+R4ph-py@users.noreply.github.com>
2022-02-15 14:11:34 +0100

    Update

--- main.py
+++ main.py
@@ -14,12 +14,44 @@ clock = pygame.time.Clock()
 
 WIN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 BACKGROUND = pygame.transform.scale(BACKGROUND, (WINDOW_WIDTH, WINDOW_HEIGHT))
+# MENU_LOGO = pygame.transform.scale(MENU_LOGO, (WINDOW_WIDTH, WINDOW_HEIGHT//8))
 
 WIN.fill(WHITE)
 pygame.display.set_caption("Menu principal")
 pygame.display.set_icon(ICON)
 
-font = pygame.font.SysFont('Corbel', WINDOW_WIDTH//25)
+buttons_font = pygame.font.SysFont('Corbel', WINDOW_WIDTH//25)
+main_text_font = pygame.font.SysFont('Landman', WINDOW_WIDTH//10)
+
+class Text:
+    """Text constructor"""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.text_color = BLACK
+        self.text = None
+        self.action = None
+
+    def set_colors(self, text_color = None):
+        """Setting colors for the button"""
+        self.text_color = text_color
+        return self
+
+    def set_text(self, text):
+        """Set text for the button"""
+        self.text = text
+        return self
+
+    def show(self):
+        """Show button"""
+
+        message = main_text_font.render(self.text, True , self.text_color)
+        WIN.blit(message , (self.x - message.get_width()/2, self.y - message.get_height()/2))
+
+    def obj_type(self):
+        """Returns the object type"""
+        return "text"
+
 
 class Button:
     """Button constructor"""
@@ -61,20 +93,22 @@ class Button:
         else:
             pygame.draw.rect(WIN, self.btn_color, [self.x, self.y, self.width, self.height])
 
-        message = font.render(self.text, True , self.text_color)
+        message = buttons_font.render(self.text, True , self.text_color)
         WIN.blit(message , (self.x + self.width/2 - message.get_width()/2, self.y + self.height/2 - message.get_height()/2))
 
-btn_h = WINDOW_HEIGHT/5
-btn_w = (16/9)*btn_h
-left_btn_x = WINDOW_WIDTH/4 - btn_w/2
-mid_btn_x = WINDOW_WIDTH/2 - btn_w/2
-right_btn_x = WINDOW_WIDTH*3/4 - btn_w/2
+    def obj_type(self):
+        """Returns the object type"""
+        return "button"
 
-top_btn_y = WINDOW_HEIGHT/16
-
-center_btn_y = WINDOW_HEIGHT/4 + WINDOW_HEIGHT/8
-
-bottom_btn_y = WINDOW_HEIGHT/2 + WINDOW_HEIGHT*3/16
+btn_h = WINDOW_HEIGHT//5
+btn_w = WINDOW_WIDTH//5
+mid_left_btn_x = WINDOW_WIDTH//3 - btn_w//2
+mid_btn_x = WINDOW_WIDTH//2 - btn_w//2
+mid_right_btn_x = WINDOW_WIDTH*2//3 - btn_w//2
+top_btn_y = WINDOW_HEIGHT//25
+center1_btn_y = top_btn_y*2 + btn_h
+center2_btn_y = top_btn_y*3 + btn_h*2
+bottom_btn_y = top_btn_y*4 + btn_h*3
 
 def socket_thread():
     """Thread function for socket"""
@@ -89,39 +123,43 @@ def launch_btn():
     """Launch button action"""
     os.system("python3 game.py")
 
-def rules_btn():
-    """Rules button action"""
-    global MAIN_MENU
-    global RULES_MENU
-    global ABOUT_MENU
-    MAIN_MENU = False
-    RULES_MENU = True
-    ABOUT_MENU = False
-
 def quit_btn():
     """Quit button action"""
     pygame.quit()
     sys.exit()
 
+def rules_btn():
+    """Rules button action"""
+    global menus
+    menus = {0: False, 1: True, 2: False}
+
 def about_btn():
     """About button action"""
-    global MAIN_MENU
-    global RULES_MENU
-    global ABOUT_MENU
-    MAIN_MENU = False
-    RULES_MENU = False
-    ABOUT_MENU = True
+    global menus
+    menus = {0: False, 1: False, 2: True}
 
-main_buttons = []
-main_buttons.append(Button(btn_w, btn_h, left_btn_x, top_btn_y).set_text("Multiplayer").set_action(launch_btn))
-main_buttons.append(Button(btn_w, btn_h, mid_btn_x, top_btn_y).set_text("Local").set_action(launch_btn))
-main_buttons.append(Button(btn_w, btn_h, mid_btn_x, center_btn_y).set_text("Règles").set_action(rules_btn))
-main_buttons.append(Button(btn_w, btn_h, mid_btn_x, bottom_btn_y).set_text("Quitter").set_action(quit_btn))
-main_buttons.append(Button(btn_w, btn_h, right_btn_x, bottom_btn_y).set_text("A propos").set_action(about_btn))
+def back_to_main():
+    """Back button action"""
+    global menus
+    menus = {0: True, 1: False, 2: False}
 
-MAIN_MENU = True
-RULES_MENU = False
-ABOUT_MENU = False
+main_menu_objs = [Button(btn_w, btn_h, mid_left_btn_x, top_btn_y).set_text("Multiplayer").set_action(multi_btn)]
+main_menu_objs.append(Button(btn_w, btn_h, mid_right_btn_x, top_btn_y).set_text("Local").set_action(launch_btn))
+main_menu_objs.append(Button(btn_w, btn_h, mid_btn_x, center1_btn_y).set_text("Règles").set_action(rules_btn))
+main_menu_objs.append(Button(btn_w, btn_h, mid_btn_x, center2_btn_y).set_text("Crédits").set_action(about_btn))
+main_menu_objs.append(Button(btn_w, btn_h, mid_btn_x, bottom_btn_y).set_text("Quitter").set_action(quit_btn))
+main_menu_objs.append(Text(200, 200).set_text(""))
+
+back_button = Button(btn_w, btn_h, mid_left_btn_x, top_btn_y).set_text("Retour").set_action(back_to_main)
+
+rules_menu_objs = [back_button]
+
+about_objs = [back_button]
+
+menus = {0: True, 1: False, 2: False} # 0 = main menu, 1 = rules menu, 2 = about menu
+
+objects = [main_menu_objs, rules_menu_objs, about_objs]
+
 while True:
     pygame.display.update()
 
@@ -134,15 +172,18 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if MAIN_MENU:
-                for button in main_buttons:
-                    if button.x <= mouse[0] <= button.x + button.width and button.y <= mouse[1] <= button.y + button.height:
-                        button.action()
+            for menu, value in menus.items():
+                if value:
+                    for dysp_obj in objects[menu]:
+                        if dysp_obj.obj_type() == "button":
+                            if dysp_obj.x <= mouse[0] <= dysp_obj.x + dysp_obj.width and dysp_obj.y <= mouse[1] <= dysp_obj.y + dysp_obj.height:
+                                dysp_obj.action()
 
     WIN.blit(BACKGROUND, (0,0))
 
-    if MAIN_MENU:
-        for button in main_buttons:
-            button.show()
+    for menu, value in menus.items():
+        if value:
+            for dysp_obj in objects[menu]:
+                dysp_obj.show()
 
     clock.tick(FPS)
--- ttmc/__init__.py
+++ ttmc/__init__.py
@@ -6,10 +6,17 @@ from screeninfo import get_monitors
 os.chdir(os.path.dirname(__file__))
 
 # Récupérer les dimensions de l'écran
-SCREEN_WIDTH = get_monitors()[0].width
-SCREEN_HEIGHT = get_monitors()[0].height
+if len(get_monitors()) == 1:
+    SCREEN_WIDTH = get_monitors()[0].width
+    SCREEN_HEIGHT = get_monitors()[0].height
 
-WINDOW_WIDTH, WINDOW_HEIGHT = SCREEN_WIDTH//2, SCREEN_HEIGHT//2
+else:
+    for monitor in get_monitors():
+        if monitor.is_primary:
+            SCREEN_WIDTH = monitor.width
+            SCREEN_HEIGHT = monitor.height
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 720, 480
 
 BOARD_WIDTH, BOARD_HEIGHT = 612, 1029
 
@@ -30,3 +37,4 @@ FPS = 60
 BACKGROUND = pygame.image.load('Background.jpg')
 BOARD = pygame.image.load('Plateau.jpg')
 ICON = pygame.image.load('icon.png')
+MENU_LOGO = pygame.image.load('logo_accueil.png')
